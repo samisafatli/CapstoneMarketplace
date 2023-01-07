1e707a141c1bb67f206541eaead57a10512a2c64
samisafatli <samisafatli@live.com>
2023-01-07 11:04:39 -0300

    Add navigation bar with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './index.scss'
-import Home from './routes/home/home'
 import { Routes, Route } from 'react-router-dom'
+
+import Home from './routes/home/home'
+import Navigation from './routes/Navigation/Navigation'
+
+import './index.scss'
+
+const Shop = () => {
+  return <h1>shopping</h1>
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+      </Route>
     </Routes>
   )
 }
